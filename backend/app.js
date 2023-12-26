60dc2b7e9d10f38b59ccdb4ae8e2487fc8edb78d
David-Ykz <kzdavid314@gmail.com>
2023-12-26 13:39:55 -0500

    created python script to load entries into sql database

--- backend/app.js
+++ backend/app.js
@@ -11,27 +11,11 @@ app.get("/message", (request, response) => {
     db.pool.query("SELECT * FROM interview_questions ORDER BY random() LIMIT 1;", function(err, res) {
         response.json({ message: res.rows[0].prompt });
     })
-
 });
-
-// app.get("/message", (req, res) => {
-//     res.json({ message: getRandomPrompt() });
-// });
 
 app.listen(8000, () => {
-    db.pool.query("SELECT * FROM interview_questions ORDER BY random() LIMIT 1;", function(err, res) {
-        console.log(res.rows[0].prompt);
-    })
-
     console.log(`Server is running on port 8000.`);
 });
-
-// function getRandomPrompt() {
-//     db.pool.query("SELECT * FROM interview_questions ORDER BY random() LIMIT 1;", function(err, res) {
-//         return res.rows[0].prompt);
-//     })
-//     return db.pool.query("SELECT * FROM interview_questions ORDER BY random() LIMIT 1;").then();
-// }
 
 
 
---
+++ backend/load_data_script.py
@@ -0,0 +1,43 @@
+file = open("interview_questions_list", "r")
+
+topic = ""
+insert_query = "INSERT INTO interview_questions(prompt, topic) values({},{});"
+apostrophe = "\'"
+
+for line in file.readlines():
+	line = line.strip()
+	if (line != ""):
+		line = line.replace("â€™","''")
+		if ("###" in line):
+			topic = line[line.find(" ") + 1:]
+
+		else:
+			line = line[line.find(" ") + 1:]
+			print(insert_query.format(apostrophe + line + apostrophe, apostrophe + topic + apostrophe))
+
+
+
+
+
+
+
+
+#topic = input("Enter the topic: ")
+#questions = []
+#readDataMode = True
+
+#while (readDataMode):
+#	string = input()
+#	if string == "exit":
+#		readDataMode = False;
+#	elif string != "":
+#		questions.append(string)
+
+
+
+
+#for entry in questions:
+#	entry = entry[entry.find(" ") + 1:]
+
+#	print("INSERT INTO interview_questions(prompt, topic) values(\'" + entry + "\', \'" + topic + "\');")
+
